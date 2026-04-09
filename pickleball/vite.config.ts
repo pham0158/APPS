3a9588ceae5e36ab8947f0c66ec3618a0e6a7af1
pham0158 <156129026+pham0158@users.noreply.github.com>
2026-04-09 13:08:53 -0500

    Update vite.config.ts

--- pickleball/vite.config.ts
+++ pickleball/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pickleball-v2/',
+  base: '/pickleball-v3/',
   plugins: [react()],
 })
